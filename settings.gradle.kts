3d57030a28a7691633f9375d4f7ffe4921b87c82
Kayo <ayushku.ak1235@gmail.com>
2025-05-28 18:17:48 +0530

    build update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "CloudstreamPlugins"
+rootProject.name = "CloudStreamExtension"
 
 // This file sets what projects are included. All new projects should get automatically included unless specified in "disabled" variable.
 
