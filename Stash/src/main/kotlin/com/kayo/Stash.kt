1f36b9f4896f94cef1a72a61c26e616a696de893
ArKanZi <ayushku.ak@gmail.com>
2025-11-25 18:30:44 +0530

    new desktop version

--- Stash/src/main/kotlin/com/kayo/Stash.kt
+++ Stash/src/main/kotlin/com/kayo/Stash.kt
@@ -186,6 +186,7 @@ class Stash : MainAPI() {
     }
 
     suspend fun stashGraphQL(bodyJson: String): String {
+        val jsonObject = gson.fromJson(bodyJson, Map::class.java)
 
 
         return app.post(
@@ -194,7 +195,7 @@ class Stash : MainAPI() {
                 "Content-Type" to "application/json",
                 "ApiKey" to apiKey
             ),
-            json = bodyJson
+            json = jsonObject
         ).text
     }
 
