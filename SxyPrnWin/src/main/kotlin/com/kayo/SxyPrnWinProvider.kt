af06e3c6d2fb74f55ec8563ef7a7c8d4d48d71ec
ArKanZi <ayushku.ak@gmail.com>
2026-03-31 17:37:50 +0530

    none

--- SxyPrnWin/src/main/kotlin/com/kayo/SxyPrnWinProvider.kt
+++ SxyPrnWin/src/main/kotlin/com/kayo/SxyPrnWinProvider.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import com.lagradost.cloudstream3.extractors.DoodstreamCom
 import com.lagradost.cloudstream3.extractors.Lulustream1
 import com.lagradost.cloudstream3.extractors.StreamTape
-import com.lagradost.cloudstream3.extractors.Vidguardto
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
@@ -17,7 +16,6 @@ class SxyPrnWinProvider : Plugin() {
         registerExtractorAPI(BigWarp())
         registerExtractorAPI(StreamTape())
         registerExtractorAPI(Lulustream1())
-        registerExtractorAPI(Vidguardto())
         registerExtractorAPI(DoodstreamCom())
     }
 }
